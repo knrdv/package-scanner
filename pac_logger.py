2c0d9841c8ec818050d8a0c9f7c782c8e1eaf3af
Konrad Viskovic <kvisko444@gmail.com>
2021-02-13 20:00:35 +0100

    Update logger

--- pac_logger.py
+++ pac_logger.py
@@ -4,8 +4,7 @@ Utility module for setting up application logging.
 
 import logging
 import sys
-
-LOG_FILE = "pacscan.log"
+import config
 
 # Set log format
 log_formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s")
@@ -15,7 +14,7 @@ console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(log_formatter)
 
 # Set file handler
-file_handler = logging.FileHandler(LOG_FILE)
+file_handler = logging.FileHandler(config.LOG_FILE)
 file_handler.setFormatter(log_formatter)
 
 logger = logging.getLogger("pacscan")
